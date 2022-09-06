f56e7945c9990bfe642ee9c8ebe11b7eed914bc7
jtduchesne <jtduchesne@gmail.com>
2022-09-06 13:03:24 -0400

    Fix: Default exports in type definition files

--- types/Cartridges.d.ts
+++ types/Cartridges.d.ts
@@ -2,5 +2,5 @@ export { Cartridge } from "./Cartridges/Cartridge.js";
 export * from "./Cartridges/FileFormats.js";
 export { Mapper } from "./Cartridges/Mapper.js";
 export { Metadata } from "./Cartridges/Metadata.js";
-export { default } from "./Cartridges/CartConnector.js";
+export { CartConnector } from "./Cartridges/CartConnector.js";
 //# sourceMappingURL=Cartridges.d.ts.map
