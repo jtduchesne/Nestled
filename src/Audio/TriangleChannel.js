1238ec8e7d1fc82ab794d26cf34e1911087f27c2
jtduchesne <jtduchesne@gmail.com>
2023-02-24 09:51:35 -0500

    Create LinearCounter base class
    - Extends `LengthCounter`.
    - Remove all counter logic from Triangle channel and make it extend this
      new base class.
    
    Also update types.

--- src/Audio/TriangleChannel.js
+++ src/Audio/TriangleChannel.js
@@ -1,4 +1,4 @@
-import LengthCounter from './LengthCounter.js';
+import LinearCounter from './LinearCounter.js';
 
 /** Output values lookup */
 const values = [
@@ -9,52 +9,26 @@ const values = [
 /**
  * Triangle channel generates a pseudo-triangle wave.
  */
-export class TriangleChannel extends LengthCounter {
+export class TriangleChannel extends LinearCounter {
     constructor() {
         super();
         
         /** @private */
         this.phase = 0;
-        
-        this.linearCounter        = 0;
-        this.linearCounterMax     = 0;
-        this.linearCounterReset   = false;
-        this.linearCounterControl = false;
     }
     
     reset() {
         super.reset();
         
         this.phase = 0;
-        
-        this.counter = 0;
-        this.linearCounter = 0;
     }
     
     //== Registers ======================================================================//
-    /** @type {number} */
-    get counter() {
-        return this.linearCounter;
-    }
-    set counter(value) {
-        if (value >= 0x80) {
-            this.lengthCounterHalt    = true;
-            this.linearCounterControl = true;
-            this.linearCounterMax     = (value - 0x80);
-        } else {
-            this.lengthCounterHalt    = false;
-            this.linearCounterControl = false;
-            this.linearCounterMax     = value;
-        }
-    }
-    
     /** @type {number} */
     get length() {
         return super.length;
     }
     set length(value) {
-        this.linearCounterReset = true;
-        
         this.timerPeriod = (this.timerPeriod & 0x0FF) | ((value & 0x07) << 8);
         
         super.length = value;
@@ -87,17 +61,6 @@ export class TriangleChannel extends LengthCounter {
                     this.phase = 0;
             }
         }
-    }
-    
-    doQuarter() {
-        if (this.linearCounterReset) {
-            this.linearCounter = this.linearCounterMax;
-        } else if (this.linearCounter > 0) {
-            this.linearCounter--;
-        }
-        
-        if (!this.linearCounterControl)
-            this.linearCounterReset = false;
     }
     
     //== Output =========================================================================//
--- types/Audio/TriangleChannel.d.ts
+++ types/Audio/TriangleChannel.d.ts
@@ -1,23 +1,15 @@
 /**
  * Triangle channel generates a pseudo-triangle wave.
  */
-export class TriangleChannel extends LengthCounter {
+export class TriangleChannel extends LinearCounter {
     /** @private */
     private phase;
-    linearCounter: number;
-    linearCounterMax: number;
-    linearCounterReset: boolean;
-    linearCounterControl: boolean;
-    set counter(arg: number);
-    /** @type {number} */
-    get counter(): number;
     /**
      * @param {number} address 16-bit address between 0x4008-0x400B
      * @param {number} data 8-bit data
      */
     writeRegister(address: number, data: number): void;
     doCycle(): void;
-    doQuarter(): void;
     /**
      * 4-bit output value
      * @type {number}
@@ -25,5 +17,5 @@ export class TriangleChannel extends LengthCounter {
     get output(): number;
 }
 export default TriangleChannel;
-import LengthCounter from "./LengthCounter.js";
+import LinearCounter from "./LinearCounter.js";
 //# sourceMappingURL=TriangleChannel.d.ts.map
