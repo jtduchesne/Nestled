eb457cca76b3058c22a6feccc5d8858814de71cb
jtduchesne <jtduchesne@gmail.com>
2023-03-02 18:42:07 -0500

    Document Mappers

--- src/Cartridges/Mappers/NROM.js
+++ src/Cartridges/Mappers/NROM.js
@@ -1,5 +1,12 @@
 import Cartridge from '../Cartridge.js';
 
+/**
+ * The generic designation *NROM* refers to the Nintendo cartridge boards *NES-NROM-128*
+ * and *NES-NROM-256*.
+ * (The suffixes 128/256 refer to kilobits by Nintendo's own designation; not kilobytes.)
+ * 
+ * The iNES format assigns *Mapper 0* to NROM.
+ */
 export class NROM extends Cartridge {
     //== CIRAM A10 (Pin22) ==============================================================//
     /**
