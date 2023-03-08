6252eef8ea58ea74b774044e65dbeae3d5f79f6b
jtduchesne <jtduchesne@gmail.com>
2023-03-07 23:55:26 -0500

    Document `CtrlConnector.js`
    Also refactor quite a bit:
    - Change `CtrlConnector.controllers` indexes to 1 / 2 and seal the
      property.
    - `.insert()`/`.remove()` now take the port as argument.
    - Add `.read()`/`.write()` as proxies between `CPU` and `Controller`.
    
    Also adjust the tests (just so that they pass, they will need work
    later on…)

--- src/CtrlConnector.js
+++ src/CtrlConnector.js
@@ -1,28 +1,56 @@
+/** @typedef {1|2} ControllerPort */
+
 import { Controller } from "./Controllers/index.js";
 
 export class CtrlConnector {
     constructor() {
-        this.controllers = [new Controller, new Controller];
+        this.controllers = Object.seal({1: new Controller, 2: new Controller});
     }
     
-    insert(controller) {
-        if (this.controllers.indexOf(controller) > -1)
-            return controller;
-        else if (this.controllers[0].empty)
-            this.controllers[0] = controller;
-        else if (this.controllers[1].empty)
-            this.controllers[1] = controller;
-        else
-            return;
-        
-        return controller;
+    //=======================================================================================//
+    /**
+     * Insert a *Controller-derived* class into given port.
+     * @param {Controller} controller
+     * @param {ControllerPort} port
+     */
+    insert(controller, port = 1) {
+        this.controllers[port] = controller;
     }
-    remove(controller) {
-        let index = this.controllers.indexOf(controller);
-        if (index > -1) {
-            this.controllers[index] = new Controller;
-            
-            return controller;
+    /**
+     * Remove the controller from the given port.
+     * @param {ControllerPort} port
+     */
+    remove(port) {
+        this.controllers[port] = new Controller;
+    }
+    
+    //=======================================================================================//
+    /**
+     * @param {number} address 16-bit address
+     * @returns {number} 5-bit value (*OR*ed with the higher 3-bits of address bus)
+     */
+    read(address) {
+        if (address === 0x4016)
+            return 0x40 + this.controllers[1].read();
+        else if (address === 0x4017)
+            return 0x40 + this.controllers[2].read();
+        else
+            return (address >>> 8) & 0xE0;
+    }
+    /**
+     * @param {number} address 16-bit value
+     * @param {number} data 3-bit value
+     */
+    write(address, data) {
+        if (data === 0x0 || data === 0x1) {
+            this.controllers[1].write(data);
+            this.controllers[2].write(data);
+            //this.expansion.write(data);
+        } else {
+            const bit0 = (data & 0x1) ? 1 : 0;
+            this.controllers[1].write(bit0);
+            this.controllers[2].write(bit0);
+            //this.expansion.write(data & 0x7);
         }
     }
 }
--- test/CtrlConnector_test.js
+++ test/CtrlConnector_test.js
@@ -5,10 +5,10 @@ import { Controller } from "../src/Controllers";
 describe("CtrlConnector", function() {
     subject(() => new CtrlConnector);
     
-    its('controllers', () => is.expected.to.be.an('array').and.have.lengthOf(2));
+    its('controllers', () => is.expected.to.be.an('object').that.has.keys(1, 2));
     
     it("has 2 empty controllers on creation", function() {
-        expect($subject.controllers[0]).to.be.an.instanceOf(Controller).and.have.property('empty', true);
         expect($subject.controllers[1]).to.be.an.instanceOf(Controller).and.have.property('empty', true);
+        expect($subject.controllers[2]).to.be.an.instanceOf(Controller).and.have.property('empty', true);
     });
 });
