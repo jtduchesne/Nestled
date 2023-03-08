50f06080b83625a382159d40706b038834dd342b
jtduchesne <jtduchesne@gmail.com>
2023-03-08 15:31:38 -0500

    Update types for controllers

--- src/Controllers/index.js
+++ src/Controllers/index.js
@@ -2,3 +2,4 @@ import * as Devices from './Devices/index.js';
 export { Devices };
 
 export { Controller } from './Controller.js';
+export { Joypad } from './Joypad.js';
--- types/Controllers/CtrlConnector.d.ts
+++
@@ -1,8 +0,0 @@
-export class CtrlConnector {
-    controllers: Controller[];
-    insert(controller: Controller): Controller | undefined;
-    remove(controller: Controller): Controller | undefined;
-}
-export default CtrlConnector;
-import Controller from "./Controller.js";
-//# sourceMappingURL=CtrlConnector.d.ts.map
