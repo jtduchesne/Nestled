ac22dd7e98e232486c88dc32b141fbde6405f4c3
jtduchesne <jtduchesne@gmail.com>
2023-03-24 19:59:07 -0400

    Document `VideoBuffer`

--- src/Video/VideoBuffer.js
+++ src/Video/VideoBuffer.js
@@ -1,22 +1,39 @@
 export class VideoBuffer {
+    /**
+     * @param {number} width
+     * @param {number} height
+     */
     constructor(width, height) {
         this.width  = width;
         this.height = height;
         
+        /** *ImageData* ready to be drawn on screen. */
         this.frame = new ImageData(width, height);
+        /** @private */
         this.image = new ImageData(width, height);
+        /** @private */
         this.data  = new Uint32Array(this.image.data.buffer);
     }
     
     //===================================================================================//
-    writePixels(x, y, values) {
-        this.data.set(values, y*this.width + x);
-    }
-    
+    /**
+     * Move the current buffer into `frame` and create a new empty buffer.
+     */
     setFrame() {
         this.frame = this.image;
         this.image = new ImageData(this.width, this.height);
         this.data  = new Uint32Array(this.image.data.buffer);
+    }
+    
+    //===================================================================================//
+    /**
+     * Write a series of pixels starting at given X/Y position (from top-left corner).
+     * @param {number} x
+     * @param {number} y
+     * @param {ArrayLike<number>} values An array of 32-bit RGBA pixel values
+     */
+    writePixels(x, y, values) {
+        this.data.set(values, y*this.width + x);
     }
 }
 
